43b9ef6427bbbe9d1cea5b8187c10dc65c80d944
niaoer <niaoer.org@gmail.com>
2012-09-12 09:34:00 +0800

    v 1.3.5
    提交最新版本

---
+++ cloudflare.php
@@ -0,0 +1,471 @@
+<?php
+/*
+Plugin Name: CloudFlare 汉化版
+Plugin URI: http://www.cloudflare.com/wiki/CloudFlareWordPressPlugin
+Description: CloudFlare 帮助您修复评论者的真实 IP 地址、保护网站不受垃圾信息干扰、优化网站数据库。<strong>注意</strong>：汉化版插件无法自动更新，请访问<a href="http://www.niaoer.org/535.html" href="_blank">鸟儿的博客</a>并手动下载后更新。
+Version: 1.3.5
+Author: Ian Pye, Jerome Chen, James Greene (CloudFlare Team)
+License: GPLv2
+*/
+
+/*
+This program is free software; you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details. 
+
+You should have received a copy of the GNU General Public License 
+along with this program; if not, write to the Free Software 
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
+
+Plugin adapted from the Akismet WP plugin.
+
+*/	
+
+define('CLOUDFLARE_VERSION', '1.3.5');
+require_once("ip_in_range.php");
+
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo "你好！我只是个插件，请不要直接调用。";
+	exit;
+}
+
+function cloudflare_init() {
+    global $cf_api_host, $cf_api_port, $is_cf;
+
+    $cf_api_host = "ssl://www.cloudflare.com";
+    $cf_api_port = 443;
+    
+    $is_cf = ($_SERVER["HTTP_CF_CONNECTING_IP"])? TRUE: FALSE;    
+
+    if (strpos($_SERVER["REMOTE_ADDR"], ":") === FALSE) {
+        $cf_ip_ranges = array("204.93.240.0/24", "204.93.177.0/24", "199.27.128.0/21", "173.245.48.0/20", "103.22.200.0/22", "141.101.64.0/18", "108.162.192.0/18","190.93.240.1/20");
+        // IPV4: Update the REMOTE_ADDR value if the current REMOTE_ADDR value is in the specified range.
+        foreach ($cf_ip_ranges as $range) {
+            if (ipv4_in_range($_SERVER["REMOTE_ADDR"], $range)) {
+                if ($_SERVER["HTTP_CF_CONNECTING_IP"]) {
+                    $_SERVER["REMOTE_ADDR"] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+                }
+                break;
+            }
+        }        
+    }
+    else {
+        $cf_ip_ranges = array("2400:cb00::/32", "2606:4700::/32", "2803:f800::/32");
+        $ipv6 = get_ipv6_full($_SERVER["REMOTE_ADDR"]);
+        foreach ($cf_ip_ranges as $range) {
+            if (ipv6_in_range($ipv6, $range)) {
+                if ($_SERVER["HTTP_CF_CONNECTING_IP"]) {
+                    $_SERVER["REMOTE_ADDR"] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+                }
+                break;
+            }
+        }
+    }
+        
+    // Let people know that the CF WP plugin is turned on.
+    if (!headers_sent()) {
+        header("X-CF-Powered-By: WP " . CLOUDFLARE_VERSION);
+    }
+    add_action('admin_menu', 'cloudflare_config_page');
+    cloudflare_admin_warnings();
+}
+add_action('init', 'cloudflare_init',1);
+
+function cloudflare_admin_init() {
+    
+}
+
+add_action('admin_init', 'cloudflare_admin_init');
+
+function cloudflare_config_page() {
+	if ( function_exists('add_submenu_page') ) {
+		add_submenu_page('plugins.php', __('CloudFlare 设置'), __('CloudFlare'), 'manage_options', 'cloudflare', 'cloudflare_conf');
+    }
+}
+
+function load_cloudflare_keys () {
+    global $cloudflare_api_key, $cloudflare_api_email;
+    if (!$cloudflare_api_key) {
+        $cloudflare_api_key = get_option('cloudflare_api_key');
+    }
+    if (!$cloudflare_api_email) {
+        $cloudflare_api_email = get_option('cloudflare_api_email');
+    }
+}
+
+function cloudflare_conf() {
+    if ( function_exists('current_user_can') && !current_user_can('manage_options') )
+        die(__('诶哟，出错了！'));
+    global $cloudflare_api_key, $cloudflare_api_email, $is_cf;
+    global $wpdb;
+
+    // get raw domain - may include www.
+    $urlparts = parse_url(site_url());
+    $raw_domain = $urlparts["host"];
+
+    $curl_installed = function_exists('curl_init');
+
+    if ($curl_installed) {
+        // Attempt to get the matching host from CF
+        $domain = get_domain($cloudflare_api_key, $cloudflare_api_email, $raw_domain);
+        // If not found, default to pulling the domain via client side.
+        if (!$domain) {
+             $domain = $raw_domain;
+        }
+    }
+    else {
+         $domain = $raw_domain;    
+    }
+    
+    define ("THIS_DOMAIN",  $domain);
+
+    $db_results = array();
+               
+	if ( isset($_POST['submit']) 
+         && !($_POST['optimize']) 
+         && check_admin_referer('cloudflare-db-api','cloudflare-db-api-nonce') ) {
+        
+		if ( function_exists('current_user_can') && !current_user_can('manage_options') ) {
+			die(__('诶哟，出错了！'));
+        }
+
+		$key = $_POST['key'];
+		$email = $_POST['email'];
+        $dev_mode = esc_sql($_POST["dev_mode"]);
+
+		if ( empty($key) ) {
+			$key_status = 'empty';
+			$ms[] = 'new_key_empty';
+			delete_option('cloudflare_api_key');
+		} else {
+            $ms[] = 'new_key_valid';
+			update_option('cloudflare_api_key', esc_sql($key));
+            update_option('cloudflare_api_key_set_once', "TRUE");
+        }
+
+		if ( empty($email) || !is_email($email) ) {
+			$email_status = 'empty';
+			$ms[] = 'new_email_empty';
+			delete_option('cloudflare_api_email');
+		} else {
+			$ms[] = 'new_email_valid';
+			update_option('cloudflare_api_email', esc_sql($email));
+            update_option('cloudflare_api_email_set_once', "TRUE");
+        }
+
+
+        $messages = array(
+                          'new_key_empty' => array('color' => 'aa0', 'text' => __('API Key 已清除')),
+                          'new_key_valid' => array('color' => '2d2', 'text' => __('您的 API Key 已验证')),
+                          'new_email_empty' => array('color' => 'aa0', 'text' => __('Email 已清除')),
+                          'new_email_valid' => array('color' => '2d2', 'text' => __('您的 Email 已验证'))
+                          );
+
+        if ($curl_installed) {
+            if ($key != "" && $email != "") {
+                set_dev_mode(esc_sql($key), esc_sql($email), THIS_DOMAIN, $dev_mode);
+                if ($dev_mode) {
+                    $ms[] = 'dev_mode_on';
+                }
+                else {
+                    $ms[] = 'dev_mode_off';
+                }
+            }
+        
+            $messages['dev_mode_on'] = array('color' => '2d2', 'text' => __('开发模式已开启！'));
+            $messages['dev_mode_off'] = array('color' => 'aa0', 'text' => __('开发模式已关闭！'));
+        }
+    } else if ( isset($_POST['submit']) 
+                && isset($_POST['optimize'])
+                && check_admin_referer('cloudflare-db-opt','cloudflare-db-opt-nonce')) {
+
+        update_option('cloudflare_api_db_last_run', time());
+        if(current_user_can('administrator')) {
+            remove_action('admin_notices', 'cloudflare_warning');
+            $tables = $wpdb->get_col("SHOW TABLES");
+            foreach($tables as $table_name) {
+                $optimize = $wpdb->query("OPTIMIZE TABLE `$table_name`");
+                $analyze = $wpdb->query("ANALYZE TABLE `$table_name`");
+                if (!$optimize || !$analyze) {
+                    $db_results[] = "优化数据表 $table_name 失败";
+                }
+            }
+            if (count($db_results) == 0) {
+                $db_results[] = "数据库优化已完成";
+            }
+        } else {
+            $db_results[] = "您的账户没有权限操作数据库 \"manage_database\"，请使用数据库管理员账户登陆后重试！";
+        }
+    }
+
+    ?>
+    <?php if ( !empty($_POST['submit'] ) && !($_POST['optimize']) ) { ?>
+    <div id="message" class="updated fade"><p><strong><?php _e('设置已更新') ?></strong></p></div>
+    <?php } else if ( isset($_POST['submit']) && isset($_POST['optimize']) ) {
+    foreach ($db_results as $res) {
+        ?><div id="message" class="updated fade"><p><strong><?php _e($res) ?></strong></p></div><?php
+    }
+} 
+    ?>
+    <div class="wrap">
+
+    <?php if ($is_cf) { ?>
+        <h3>您正在使用 CloudFlare！</h3>
+    <?php } ?>
+
+    <h4><?php _e('CLOUDFLARE WORDPRESS 插件:'); ?></h4>
+        <?php //    <div class="narrow"> ?>
+
+CloudFlare 是一款专门为 WordPress 开发的插件，它的功能主要有：
+<ol>
+<li>修正评论者的 IP 地址</li>
+<li>保护您的网站不受垃圾信息干扰</li>
+<li>优化您的网站数据库（可选）</li>
+</ol>
+
+<h4>适用版本：</h4>
+
+目前兼容 WordPress 2.8.6 到 WordPress 3.4，如果您正在使用的版本低于 2.8.6，将无法使用本插件，建议您升级到最新版本的 WordPress。
+
+<h4>说明：</h4>
+
+<ol>
+<li>由于 CloudFlare 使用反向代理的方式导致评论者的真实 IP 地址变成了 CloudFlare 的 IP 地址，所以该插件能将 IP 地址修正为真实地址。</li>
+ 
+<li>使用数据库优化工具时，您的网站暂时无法添加、修改文章或页面，评论功能也暂时无法使用，直到优化结束后恢复。一般来说优化过程使用的时间非常短暂。</li>
+
+<li>如果您将某条评论标记为垃圾内容，那么这条评论内容将会发送到 CloudFlare，帮助您的网站得到更好的保护。</li>
+
+<li>我们强烈推荐 CloudFlare 用户安装此插件。</li>
+
+<li>注意：本插件与 Akismet 和 W3 Total Cache 兼容，我们推荐您继续使用它们。</li> 
+
+</ol>
+
+<h4>更多：</h4>
+
+<a href="http://www.cloudflare.com/" href="_blank">CloudFlare 官方网站</a>
+
+    <?php 
+        if ($curl_installed) {
+            $dev_mode = get_dev_mode_status($cloudflare_api_key, $cloudflare_api_email, THIS_DOMAIN);
+        }
+    ?>
+
+    <hr />
+
+    <form action="" method="post" id="cloudflare-conf">
+    <?php wp_nonce_field('cloudflare-db-api','cloudflare-db-api-nonce'); ?>
+    <?php if (get_option('cloudflare_api_key') && get_option('cloudflare_api_email')) { ?>
+    <?php } else { ?> 
+        <p><?php printf(__('输入您的 API Key，获取 API Key 请登陆<a href="%1$s">CloudFlare</a>后进入 \'Account\' 查看。'), 'https://www.cloudflare.com/my-account.html'); ?></p>
+    <?php } ?>
+    <?php if ($ms) { foreach ( $ms as $m ) { ?>
+    <p style="padding: .5em; color: #<?php echo $messages[$m]['color']; ?>; font-weight: bold;"><?php echo $messages[$m]['text']; ?></p>
+    <?php } } ?>
+    <h3><label for="key"><?php _e('CloudFlare API Key'); ?></label></h3>
+    <p><input id="key" name="key" type="text" size="50" maxlength="48" value="<?php echo get_option('cloudflare_api_key'); ?>" style="font-family: 'Courier New', Courier, mono; font-size: 1.5em;" /> (<?php _e('<a href="https://www.cloudflare.com/my-account.html" target="_blank">获取 API Key</a>'); ?>)</p>
+    <h3><label for="email"><?php _e('CloudFlare 账户邮箱'); ?></label></h3>
+    <p><input id="email" name="email" type="text" size="50" maxlength="48" value="<?php echo get_option('cloudflare_api_email'); ?>" style="font-family: 'Courier New', Courier, mono; font-size: 1.5em;" /> (<?php _e('<a href="https://www.cloudflare.com/my-account.html" target="_blank">获取邮箱</a>'); ?>)
+    <h3><label for="dev_mode"><?php _e('开发模式'); ?></label> <span style="font-size:9pt;">(<a href="http://support.cloudflare.com/kb/what-do-the-various-cloudflare-settings-do/what-does-cloudflare-development-mode-mean" " target="_blank">这是什么？</a>)</span></h3>
+
+    <? if ($curl_installed) { ?>
+    <div style="font-family: 'Courier New', Courier, mono; font-size: 1.5em;">
+    <input type="radio" name="dev_mode" value="0" <? if ($dev_mode == "off") echo "checked"; ?>> 关闭
+    <input type="radio" name="dev_mode" value="1" <? if ($dev_mode == "on") echo "checked"; ?>> 开启
+    </div>
+    <? } else { ?>
+    You cannot toggle development mode because cURL is not installed for your domain.  Please contact a server administrator for assistance with installing cURL.
+    <? } ?>
+    
+    </p>
+    <p class="submit"><input type="submit" name="submit" value="<?php _e('更新设置 &raquo;'); ?>" /></p>
+    </form>
+
+    <form action="" method="post" id="cloudflare-db">
+    <?php wp_nonce_field('cloudflare-db-opt','cloudflare-db-opt-nonce'); ?>
+    <input type="hidden" name="optimize" value="1" />
+
+    <h4><label for="optimize_db"><?php _e('数据库优化（可选）：'); ?></label>
+    <input type="submit" name="submit" value="<?php _e('优化数据库'); ?>" /> (<?php _e('<a href="http://www.cloudflare.com/wiki/WordPressDBOptimizer">这是什么？</a>'); ?>)</h4>
+
+    </form>
+
+        <?php //    </div> ?>
+    </div>
+    <?php
+}
+
+function cloudflare_admin_warnings() {
+    
+    global $cloudflare_api_key, $cloudflare_api_email; 
+    load_cloudflare_keys();
+
+    /**
+	if ( !get_option('cloudflare_api_key_set_once') && !$cloudflare_api_key && !isset($_POST['submit']) ) {
+		function cloudflare_warning() {
+			echo "
+			<div id='cloudflare-warning' class='updated fade'><p><strong>".__('CloudFlare 插件安装完成，')."</strong> ".sprintf(__('您还需 <a href="%1$s">输入您的 CloudFlare API key</a> 后才可使用'), "plugins.php?page=cloudflare")。"</p></div>
+			";
+		}
+		add_action('admin_notices', 'cloudflare_warning');
+		return;
+	} else if ( !get_option('cloudflare_api_key_set_once') && !$cloudflare_api_email && !isset($_POST['submit']) ) {
+		function cloudflare_warning() {
+			echo "
+			<div id='cloudflare-warning' class='updated fade'><p><strong>".__('CloudFlare 插件安装完成，')."</strong> ".sprintf(__('您还需<a href="%1$s">输入您的 CloudFlare 账户邮箱</a> 后才可使用。'), "plugins.php?page=cloudflare")."</p></div>
+			";
+		}
+		add_action('admin_notices', 'cloudflare_warning');
+		return;
+	} 
+    */
+    
+    // Check to see if they should optimized their DB
+    $last_run_time = (int)get_option('cloudflare_api_db_last_run');
+    if (!$last_run_time) {
+        $last_run_time = time();
+    }
+    if (time() - $last_run_time > 5259487) { // 2 Months (avg)
+        function cloudflare_warning() {
+			echo "
+			<div id='cloudflare-warning' class='updated fade'><p><strong>".__('您的数据库应该优化一下')."</strong> ".sprintf(__('我们建议您每两个月使用 <a href="%1$s">CloudFlare 数据库优化工具</a>。'), "plugins.php?page=cloudflare")."</p></div>
+			";
+		}
+		add_action('admin_notices', 'cloudflare_warning');
+		return;
+    }
+}
+
+// Now actually allow CF to see when a comment is approved/not-approved.
+function cloudflare_set_comment_status($id, $status) {
+    global $cf_api_host, $cf_api_port, $cloudflare_api_key, $cloudflare_api_email; 
+    if (!$cf_api_host || !$cf_api_port) {
+        return;
+    }
+    load_cloudflare_keys();
+    if (!$cloudflare_api_key || !$cloudflare_api_email) {
+        return;
+    }
+
+    // ajax/external-event.html?email=ian@cloudflare.com&t=94606855d7e42adf3b9e2fd004c7660b941b8e55aa42d&evnt_v={%22dd%22:%22d%22}&evnt_t=WP_SPAM
+    $comment = get_comment($id);
+    $value = array("a" => $comment->comment_author, 
+                   "am" => $comment->comment_author_email,
+                   "ip" => $comment->comment_author_IP,
+                   "con" => substr($comment->comment_content, 0, 100));
+    $url = "/ajax/external-event.html?evnt_v=" . urlencode(json_encode($value)) . "&u=$cloudflare_api_email&tkn=$cloudflare_api_key&evnt_t=";
+     
+    // If spam, send this info over to CloudFlare.
+    if ($status == "spam") {
+        $url .= "WP_SPAM";
+        $fp = @fsockopen($cf_api_host, $cf_api_port, $errno, $errstr, 30);
+        if ($fp) {
+            $out = "GET $url HTTP/1.1\r\n";
+            $out .= "Host: www.cloudflare.com\r\n";
+            $out .= "Connection: Close\r\n\r\n";
+            fwrite($fp, $out);
+            $res = "";
+            while (!feof($fp)) {
+                $res .= fgets($fp, 128);
+            }
+            fclose($fp);
+        }
+    }
+}
+
+add_action('wp_set_comment_status', 'cloudflare_set_comment_status', 1, 2);
+
+function get_dev_mode_status($token, $email, $zone) {
+    $url = 'https://www.cloudflare.com/api_json.html';
+    $fields = array(
+                'a'=>"zone_load",
+                'tkn'=>$token,
+                'email'=>$email,
+                'z'=>$zone
+              );
+
+    foreach($fields as $key=>$value) { 
+        $fields_string .= $key.'='.$value.'&';
+    }
+    rtrim($fields_string,'&');
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_POST,count($fields));
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $result = curl_exec($ch);
+    $result = json_decode($result);
+    curl_close($ch);
+
+    if ($result->response->zone->obj->zone_status_class == "status-dev-mode") {
+        return "on";
+    }
+
+    return "off";
+}
+
+function set_dev_mode($token, $email, $zone, $value)
+{
+    $url = 'https://www.cloudflare.com/api_json.html';
+    $fields = array(
+            'a'=>"devmode",
+            'tkn'=>$token,
+            'email'=>$email,
+            'z'=>$zone,
+            'v'=>$value
+          );
+    foreach($fields as $key=>$value) { 
+        $fields_string .= $key.'='.$value.'&';
+    }
+    rtrim($fields_string,'&');
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_POST,count($fields));
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $result = curl_exec($ch);
+    $result = json_decode($result);
+    curl_close($ch);
+}
+
+function get_domain($token, $email, $raw_domain) {
+    $url = 'https://www.cloudflare.com/api_json.html';
+    $fields = array(
+                'a'=>"zone_load_multi",
+                'tkn'=>$token,
+                'email'=>$email
+              );
+
+    foreach($fields as $key=>$value) { 
+        $fields_string .= $key.'='.$value.'&';
+    }
+    rtrim($fields_string,'&');
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_POST,count($fields));
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $result = curl_exec($ch);
+    $result = json_decode($result);
+    curl_close($ch);
+
+    $zone_count = $result->response->zones->count;
+    if ($zone_count > 0) {
+        for ($i = 0; $i < $zone_count; $i++) {
+            $zone_name = $result->response->zones->objs[$i]->zone_name;
+            if (strpos($raw_domain, $zone_name) !== FALSE){
+                return $zone_name;
+            }
+        }
+    }
+    
+    return null;
+}
+?>
